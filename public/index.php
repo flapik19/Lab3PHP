926988dc702a7a7b0401349ef018dc05b18f64c0
Flapik <selivanovvlad3211@gmail.com>
2024-11-13 23:51:12 +0300

    someCode

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php 
 require 'vendor/autoload.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> lab3PHP </title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Сюда попадете после того, как пройдете регистрацию или войдете в аккаунт.</h1>
+    </body>
+</html>
---
+++ public/src/pages/login.php
@@ -0,0 +1,35 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <meta charset = "UTF-8">
+        <link rel = "stylesheet" type = "text/css" href="styles/login.css">
+    </head>
+    <body>
+        <div class = "mainContain">
+            <h1>Registration</h1>
+            <form action ="" method = "post" >
+                <input type = "text" id = "name" placeholder="Type your name" required>
+            </form>
+            <form action ="" method = "post" >
+                <input type = "password" id = "password" placeholder="Type your password" required>
+            </form>
+            <form action ="" method = "post" ></form>
+                <input type = "password" id = "RepeatPassword" placeholder="Repeat your password" required>
+            </form>
+            
+            <form action ="" method = "post" ></form>
+                <input type = "text" id = "bgColor" placeholder="Enter bgColor" required>
+            </form>
+
+            <form action ="" method = "post" ></form>
+                <input type = "text" id = "textColor" placeholder="Enter font color" required>
+            </form>
+            <button class = "registBut">Registration</button>
+        </div>
+    </body> 
+</html>
